825decd4681ec7f6f7403855fd93f289646dbce9
Sully Birashk <sully.birashk@gmail.com>
2022-06-13 20:17:06 -0600

    Refactor: Make call singular to find specific state

--- app/facades/gas_facade.rb
+++ app/facades/gas_facade.rb
@@ -1,8 +1,7 @@
 class GasFacade
 
   def self.find_gas_prices(state)
-    data = GasService.get_state_prices(state)
-    prices = Gas.new(data)
+    data = GasService.get_state_price(state)
   end
 
 end
