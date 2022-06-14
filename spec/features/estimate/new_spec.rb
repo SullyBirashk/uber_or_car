2e083a040a9dc6911e3356b9d88c8a5648b469f8
Sully Birashk <sully.birashk@gmail.com>
2022-06-13 20:08:48 -0600

    Refactor: Change Welcome Controller to Estimate Controller Refac: Change index to new action

--- spec/features/estimate/new_spec.rb
+++ spec/features/estimate/new_spec.rb
@@ -1,19 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe 'Welcome Page' do
- before :each do
- end
+RSpec.describe 'Estimate Page' do
 
  it 'exists' do
-    visit '/welcome'
+    visit '/estimate/new'
 
     fill_in('Monthly car payment', with: 330)
     fill_in('Insurance cost', with: 600)
     fill_in('Miles driven this month', with: 1200)
-    fill_in('State you live in', with: 'CO')
+    fill_in('State you live in', with: 'Colorado')
     click_button('Calculate Price')
 
-    expect(current_path).to eq('/welcome')
+    expect(current_path).to eq('/estimate')
 
  end
 end
