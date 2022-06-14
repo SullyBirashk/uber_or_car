38e24bfbce33d2b972b2a24712c573870be20a0b
Sully Birashk <sully.birashk@gmail.com>
2022-06-13 20:07:18 -0600

    Refactor: Poro Test data is simpler

--- spec/poros/gas_spec.rb
+++ spec/poros/gas_spec.rb
@@ -2,20 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Gas do
   it "exists with attributes" do
-    gas_data = {:success=>true,
- :result=>
-  {:state=>{:currency=>"usd", :name=>"Colorado", :lowerName=>"colorado", :gasoline=>"4.727", :midGrade=>"5.040", :premium=>"5.330", :diesel=>"5.468"},
-   :cities=>
-    [{:currency=>"usd", :gasoline=>"4.721", :midGrade=>"5.038", :premium=>"5.374", :diesel=>"5.432", :name=>"Boulder-Longmont", :lowerName=>"boulder-longmont"
-      }]}}
+    gas_data = {:currency=>"usd", :name=>"Colorado", :gasoline=>"4.875", :midGrade=>"5.204", :premium=>"5.500", :diesel=>"5.516"}
 
 
     gas = Gas.new(gas_data)
     expect(gas).to be_a Gas
     expect(gas.name).to be_a String
     expect(gas.regular).to be_a String
-    expect(gas.medium).to be_a String
-    expect(gas.premium).to be_a String
-    expect(gas.diesel).to be_a String
   end
 end
